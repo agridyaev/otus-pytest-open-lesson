4af240b874058f7ce26bff46db7f4bbdf09d28e1
Andrey Gridyaev <agridyaev@gmail.com>
2021-03-20 14:24:21 +0300

    03_fixtures: add indirect parametrization example

---
+++ tests/03_fixtures/test_07_indirect_parametrize.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.fixture
+def fixt(request):
+    return request.param * 3
+
+
+@pytest.mark.parametrize("fixt", ["a", "b"], indirect=True)
+def test_indirect(fixt):
+    assert len(fixt) == 3
+
+
+@pytest.fixture(scope="function")
+def x(request):
+    return request.param * 3
+
+
+@pytest.fixture(scope="function")
+def y(request):
+    return request.param * 2
+
+
+@pytest.mark.parametrize("x, y", [("a", "b")], indirect=["x"])
+def test_partial_indirect(x, y):
+    assert x == "aaa"
+    assert y == "b"
